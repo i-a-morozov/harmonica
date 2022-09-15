aa3b444fc094e9141f0a366e09f15ec7bd2f9dbc
Ivan Morozov <i.a.morozov@inp.nsk.su>
2022-09-15 10:25:45 +0700

    15_09_2022: accumulated phase in table

--- harmonica/table.py
+++ harmonica/table.py
@@ -4,6 +4,7 @@ Generate contaner with estimated parameters (frequencies, amplitudes and phases)
 
 """
 
+import numpy
 import torch
 
 from .decomposition import Decomposition
@@ -42,6 +43,8 @@ class Table():
         x phase error
     sigma_fy: torch.Tensor:
         y phase error
+    x & y: torch.Tensor
+        x & y data
     dtype: torch.dtype
         data type
     device: torch.device
@@ -85,6 +88,10 @@ class Table():
         y phase advance from each location to the next one
     sigma_y: torch.Tensor
         y phase advance error from each location to the next one
+    x & y: torch.Tensor
+        x & y data
+    accumulate: bool
+        flag to accumulate phase
     dtype: torch.dtype
         data type
     device: torch.device
@@ -92,9 +99,11 @@ class Table():
 
     Methods
     ----------
-    __init__(self, name:list, nux:torch.Tensor, nuy:torch.Tensor, ax:torch.Tensor, ay:torch.Tensor, fx:torch.Tensor, fy:torch.Tensor, sigma_nux:torch.Tensor=None, sigma_nuy:torch.Tensor=None, sigma_ax:torch.Tensor=None, sigma_ay:torch.Tensor=None, sigma_fx:torch.Tensor=None, sigma_fy:torch.Tensor=None, *, dtype:torch.dtype=torch.float64, device:torch.device='cpu') -> None
+    __init__(self, name:list, nux:torch.Tensor, nuy:torch.Tensor, ax:torch.Tensor, ay:torch.Tensor, fx:torch.Tensor, fy:torch.Tensor, sigma_nux:torch.Tensor=None, sigma_nuy:torch.Tensor=None, sigma_ax:torch.Tensor=None, sigma_ay:torch.Tensor=None, sigma_fx:torch.Tensor=None, sigma_fy:torch.Tensor=None, *, x:torch.Tensor=None, y:torch.Tensor=None, dtype:torch.dtype=torch.float64, device:torch.device='cpu') -> None
         Table instance initialization.
-    def __repr__(self) -> str
+    accumulate_phase(self) -> None
+        Accumulate x & y phases (modify self.fx and self.fy).
+    __repr__(self) -> str
         String representation.
 
     """
@@ -111,7 +120,11 @@ class Table():
                  sigma_ax:torch.Tensor=None,
                  sigma_ay:torch.Tensor=None,
                  sigma_fx:torch.Tensor=None,
-                 sigma_fy:torch.Tensor=None, *,
+                 sigma_fy:torch.Tensor=None,
+                 *,
+                 x:torch.Tensor=None,
+                 y:torch.Tensor=None,
+                 accumulate:bool=False,
                  dtype:torch.dtype=torch.float64,
                  device:torch.device='cpu') -> None:
         """
@@ -145,6 +158,10 @@ class Table():
             x phase error
         sigma_fy: torch.Tensor:
             y phase error
+        x & y: torch.Tensor
+            x & y data
+        accumulate: bool
+            flag to accumulate phase
         dtype: torch.dtype
             data type
         device: torch.device
@@ -168,6 +185,9 @@ class Table():
         self.fx = fx.to(self.dtype).to(self.device)
         self.fy = fy.to(self.dtype).to(self.device)
 
+        if accumulate:
+            self.accumulate_phase()
+
         self.sigma_nux = torch.tensor(0.0, dtype=dtype, device=device) if sigma_nux is None else sigma_nux.to(self.dtype).to(self.device)
         self.sigma_nuy = torch.tensor(0.0, dtype=dtype, device=device) if sigma_nuy is None else sigma_nuy.to(self.dtype).to(self.device)
 
@@ -182,6 +202,30 @@ class Table():
         other = probe + 1
         self.phase_x, self.sigma_x = Decomposition.phase_advance(probe, other, self.nux, self.fx, error=True, sigma_frequency=self.sigma_nux, sigma_phase=self.sigma_fx, model=False)
         self.phase_y, self.sigma_y = Decomposition.phase_advance(probe, other, self.nuy, self.fy, error=True, sigma_frequency=self.sigma_nuy, sigma_phase=self.sigma_fy, model=False)
+
+        self.x, self.y = x, y
+
+
+    def accumulate_phase(self) -> None:
+        """
+        Accumulate x & y phases (modify self.fx and self.fy).
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+
+        """
+        for index in range(1, self.size):
+
+            while self.fx[index] < self.fx[index - 1]:
+                self.fx[index:] += 2.0*numpy.pi
+
+            while self.fy[index] < self.fy[index - 1]:
+                self.fy[index:] += 2.0*numpy.pi
 
 
     def __repr__(self) -> str:
